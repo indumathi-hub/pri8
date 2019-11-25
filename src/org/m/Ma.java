a979920e0b0cc98c3eb96671caf46393dd4dffe2
indumathi-hub <indurag77@gmail.com>
2019-11-26 02:09:52 +0530

    new

--- src/org/m/Ma.java
+++ src/org/m/Ma.java
@@ -3,7 +3,8 @@ package org.m;
 public class Ma {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("test1");
+		System.out.println("test234");
+		System.out.println("test");
 	}
 
 }
